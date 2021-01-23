17c2db2f3cc250aa564920871e27e271a03895e1
EC2 Default User <ec2-user@ip-172-31-71-211.ec2.internal>
2021-01-23 16:28:42 +0000

    current_user.tasks.build　の削除、session/newアクションに対応するroutingの削除

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,7 +3,7 @@ class TasksController < ApplicationController
   before_action :correct_user, only: [:show, :edit, :update, :destroy]
   
   def index
-    @task = current_user.tasks.build  # form_with 用
+    # @task = current_user.tasks.build  # ? TODO: .buildの質問
     @tasks = current_user.tasks # order(id: :desc).page(params[:page])
   end
 
